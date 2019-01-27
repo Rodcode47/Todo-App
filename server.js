4afbdab7bf5814a2df6de6804770a798bde08ba9
rodney47 <rodney.hammad@gmail.com>
2019-01-28 00:14:59 +0200

    first commit to netlify

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
- const app = express();
+const path = require('path');
 
- app.use(express.static(__dirname + '/dist'));
- app.listen(process.env.PORT || 8080);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
