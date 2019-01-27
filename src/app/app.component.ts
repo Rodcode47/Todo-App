dc053fbb4ec665945961bd383d3bb01c6bcaced7
rodney47 <rodney.hammad@gmail.com>
2019-01-27 18:53:38 +0200

    removed restriction to login or signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { moveIn, fallIn, moveInLeft } from './router.animations';
 
-var http = require("http");
-setInterval(function() {
-    http.get("https://ngrax5-todo-app.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
